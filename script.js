11458b4b145b86f984bbed61047bd703f5ae2029
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-08-28 00:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let ct = 0;
+const counter = document.getElementById("counter");
+// incrementBtn.addEventListener("click", (){
+// 	alert(ct+1);
+// })
+
+function clicked(){
+	ct = ct + 1;
+	alert(ct);
+	counter.innerText = ct;
+}
